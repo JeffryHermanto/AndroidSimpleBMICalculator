c97b07b8acbaaf878a601e8bce1b9e6bf87cd70b
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-04 11:05:05 +0700

    Refactor findViews and setupButtonClickListener

--- app/src/main/java/com/jeffryhermanto/bmicalculator/MainActivity.java
+++ app/src/main/java/com/jeffryhermanto/bmicalculator/MainActivity.java
@@ -3,25 +3,52 @@ package com.jeffryhermanto.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private EditText ageEditText;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText weightEditText;
+    private Button calculateButton;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setupButtonClickListener();
+    }
 
-        RadioButton maleButton = findViewById(R.id.radio_button_male);
-        RadioButton femaleButton = findViewById(R.id.radio_button_female);
-        EditText ageEditText = findViewById(R.id.edit_text_age);
-        EditText feetEditText = findViewById(R.id.edit_text_feet);
-        EditText inchesEditText = findViewById(R.id.edit_text_inches);
-        EditText weightEditText = findViewById(R.id.edit_text_weight);
-        Button calculateButton = findViewById(R.id.button_calculate);
-        TextView resultText = findViewById(R.id.text_view_result);
+    private void findViews() {
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radio_button_female);
+        ageEditText = findViewById(R.id.edit_text_age);
+        feetEditText = findViewById(R.id.edit_text_feet);
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        weightEditText = findViewById(R.id.edit_text_weight);
+        calculateButton = findViewById(R.id.button_calculate);
+        resultText = findViewById(R.id.text_view_result);
+    }
+
+    private void setupButtonClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBmi();
+            }
+        });
+    }
+
+    private void calculateBmi() {
+        Toast.makeText(MainActivity.this, "Calculating...", Toast.LENGTH_LONG).show();
     }
 }
