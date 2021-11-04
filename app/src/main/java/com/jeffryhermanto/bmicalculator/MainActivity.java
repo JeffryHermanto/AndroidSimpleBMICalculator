ccda218418cb6686b79e9744ac19ae40ef3b8a2f
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-04 09:17:22 +0700

    Add id on each element in activity_main.xml

--- app/src/main/java/com/jeffryhermanto/bmicalculator/MainActivity.java
+++ app/src/main/java/com/jeffryhermanto/bmicalculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jeffryhermanto.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String alertText = "This is my alert text";
-
-        Toast.makeText(this, alertText, Toast.LENGTH_LONG).show();
+        RadioButton maleButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleButton = findViewById(R.id.radio_button_female);
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText feetEditText = findViewById(R.id.edit_text_feet);
+        EditText inchesEditText = findViewById(R.id.edit_text_inches);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+        Button calculateButton = findViewById(R.id.button_calculate);
+        TextView resultText = findViewById(R.id.text_view_result);
     }
 }
